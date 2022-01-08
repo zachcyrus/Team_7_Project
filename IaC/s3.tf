41aa37254448a351fbef268352d8c14492ab18f7
Brittney <brittney.r.jones519@gmail.com>
2022-01-07 20:40:20 -0400

    Adding sse to S3 bucket

--- IaC/s3.tf
+++ IaC/s3.tf
@@ -4,8 +4,16 @@
 resource "aws_s3_bucket" "terraform_state_s3" {
   bucket = "mafia-app-terraform-statefile" 
   force_destroy = true
-# Enable versioning to see history of our state files
+# Enabling versioning to see history of our state files
   versioning {
          enabled = true
         }
+# Enabling server-side encryption
+server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
