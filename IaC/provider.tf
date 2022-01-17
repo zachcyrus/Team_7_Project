f8f40f39cd166679d8b3cfd4170de16a02aea928
Brittney <brittney.r.jones519@gmail.com>
2022-01-17 16:45:23 -0500

    updating provider.tf

--- IaC/provider.tf
+++ IaC/provider.tf
@@ -1,15 +1,14 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "3.72.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.72.0"
+    }
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
-  version = "~> 3.0"
   region  = "us-east-1"
   default_tags {
     tags = {
