7c15eafe8960e857b3e324666a07c04d98d62efc
Brittney <brittney.r.jones519@gmail.com>
2022-01-17 11:41:20 -0500

    Updating VPC and ALB

--- IaC/alb.tf
+++ IaC/alb.tf
@@ -6,8 +6,23 @@
 #   internal           = false
 #   subnets            = module.vpc.public_subnets
 #   tags = {
-#     "env"       = "dev"
-#     "createdBy" = "mkerimova"
 #   }
 #   security_groups = [aws_security_group.lb.id]
+# }
+
+# resource "aws_security_group" "lb" {
+#   name   = "allow-all-lb"
+#   vpc_id = data.aws_vpc.main.id
+#   ingress {
+#     from_port   = 5000
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 # }
