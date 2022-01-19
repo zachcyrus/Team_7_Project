23e1b1f1c6d6b2bf5e573b6020060c3239af309a
Brittney <brittney.r.jones519@gmail.com>
2022-01-19 14:38:51 -0500

    updating ecs.tf

--- IaC/ecs.tf
+++ IaC/ecs.tf
@@ -45,12 +45,12 @@ resource "aws_ecs_service" "Mafia-ecs-service" {
 # EOF
 # }
 
-
+#Creating Task Definition
 resource "aws_ecs_task_definition" "MafiaApp-ecs-task-definition" {
   family                   = "MafiaAppTestDefinition"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  execution_role_arn       = "arn:aws:iam::649474668035:role/ecsTaskExecutionRole"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   cpu                      = 1024
   memory                   = 2048
   container_definitions    = <<TASK_DEFINITION
@@ -75,4 +75,51 @@ TASK_DEFINITION
     operating_system_family = "LINUX"
     cpu_architecture        = "ARM64"
   }
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name   = "ecs-task-sg"
+  vpc_id = module.vpc.vpc_id
+ 
+  ingress {
+   protocol         = "tcp"
+   from_port        = 5000
+   to_port          = 5000
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+ 
+  egress {
+   protocol         = "-1"
+   from_port        = 0
+   to_port          = 0
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "MafiaApp-ecsTaskExecutionRole"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
