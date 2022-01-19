ea361a3e804410aff1ed64d3ac71e6cc1ca10086
Brittney <brittney.r.jones519@gmail.com>
2022-01-19 16:17:17 -0500

    updating alb.tf

--- IaC/alb.tf
+++ IaC/alb.tf
@@ -1,28 +1,28 @@
 
 
-# resource "aws_lb" "test-lb" {
-#   name               = "test-ecs-lb"
-#   load_balancer_type = "application"
-#   internal           = false
-#   subnets            = module.vpc.public_subnets
-#   tags = {
-#   }
-#   security_groups = [aws_security_group.lb.id]
-# }
+resource "aws_lb" "MafiaApp-lb" {
+  name               = "MafiaApp-ecs-lb"
+  load_balancer_type = "application"
+  internal           = false
+  subnets            = module.vpc.public_subnets
+  tags = {
+  }
+  security_groups = [aws_security_group.lb.id]
+}
 
-# resource "aws_security_group" "lb" {
-#   name   = "allow-all-lb"
-#   vpc_id = data.aws_vpc.main.id
-#   ingress {
-#     from_port   = 5000
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_security_group" "lb" {
+  name   = "allow-all-lb"
+  vpc_id = data.aws_vpc.main.id
+  ingress {
+    from_port   = 5000
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
