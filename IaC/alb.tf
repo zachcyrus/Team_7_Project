2f896e41a213ec20607066709d5d82b11529f3e7
Brittney <brittney.r.jones519@gmail.com>
2022-01-19 16:26:00 -0500

    updating alb.tf

--- IaC/alb.tf
+++ IaC/alb.tf
@@ -15,8 +15,8 @@ resource "aws_security_group" "lb" {
   vpc_id = data.aws_vpc.main.id
   ingress {
     from_port   = 5000
-    to_port     = 0
-    protocol    = "-1"
+    to_port     = 5000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
